15b5bd068ae071291f9782beac2f932bfa94c6a4
Lyubomir Ganev <luboganev@gmail.com>
2020-06-14 17:04:49 +0200

    Add text controllers and focus handling

--- lib/page/add_bookmark_page.dart
+++ lib/page/add_bookmark_page.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 
-class AddBookmarkPage extends StatelessWidget {
+import '../model/bookmark.dart';
+
+class AddBookmarkPage extends StatefulWidget {
+  @override
+  _AddBookmarkPageState createState() => _AddBookmarkPageState();
+}
+
+class _AddBookmarkPageState extends State<AddBookmarkPage> {
+  final _titleTextController = TextEditingController();
+
+  final _linkTextController = TextEditingController();
+
+  final _linkFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add a new Bookmark"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              maxLines: 1,
+              autofocus: true,
+              controller: _titleTextController,
+              textInputAction: TextInputAction.next,
+              onSubmitted: (value) {
+                FocusScope.of(context).requestFocus(_linkFocusNode);
+              },
+              decoration: InputDecoration(
+                  icon: Icon(Icons.title),
+                  labelText: "Title",
+                  hintText: "Title of the bookmark",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextField(
+              maxLines: 1,
+              enableSuggestions: true,
+              controller: _linkTextController,
+              focusNode: _linkFocusNode,
+              textInputAction: TextInputAction.done,
+              decoration: InputDecoration(
+                  icon: Icon(Icons.link),
+                  labelText: "Link",
+                  hintText: "Webpage link",
+                  border: OutlineInputBorder()),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: () {},
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _titleTextController.dispose();
+    _linkTextController.dispose();
+    _linkFocusNode.dispose();
+    super.dispose();
   }
 }
